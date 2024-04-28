3dd07a05b59738ec9efab54bb22828d656d614c6
noahkuskye1 <111530647+noahkuskye1@users.noreply.github.com>
2024-04-28 01:16:32 +0000

    Update FifthPage.php

--- FifthPage.php
+++ FifthPage.php
@@ -44,43 +44,55 @@
           </div>
           <div style="background-color:#ffffff;
           padding:15px;text-align:center;">
-           <div id="txt"></div>
+          <div id="clock">&nbsp;</div>
+           <?php
+                date_default_timezone_set('UTC');
+            ?>
+          <script>
+            var d = new Date(<?php echo time() * 1000 ?>);
 
-           <script>
-           function startTime() {
-             const today = new Date();
-             let h = today.getHours();
-             let m = today.getMinutes();
-             let s = today.getSeconds();
-             let session = "AM";
-             m = checkTime(m);
-             s = checkTime(s);
-             if (h==0){
-              h=12;
-             }
-             if (h > 12){
-              h=h-12;
-              session = "PM";
-             }
-             document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s + " " + session;
-             setTimeout(startTime, 1000);
-           }
-           
-           function checkTime(i) {
-             if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-             return i;
-           }
-           </script>
+            function updateClock() {
+              // Increment the date
+              d.setTime(d.getTime() + 1000);
+
+              // Translate time to pieces
+              var currentHours = d.getHours();
+              var currentMinutes = d.getMinutes();
+              var currentSeconds = d.getSeconds();
+
+              // Add the beginning zero to minutes and seconds if needed
+              currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+              currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+              // Determine the meridian
+              var meridian = (currentHours < 12) ? "am" : "pm";
+
+              // Convert the hours out of 24-hour time
+              currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+              currentHours = (currentHours == 0) ? 12 : currentHours;
+
+              // Generate the display string
+              var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + meridian;
+
+              // Update the time
+              document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+              }
+
+              window.onload = function() {
+              updateClock();
+              setInterval('updateClock()', 1000);
+              }
+          </script>
          </div>
         <!-- navigation bar -->
         <div class="topnav">
             <nav>
-                <a href="Webpage1.html">Home page</a>
-                <a href="FirstPage.html">First Page</a> 
-                <a href="SecondPage.html">Second Page</a> 
-                <a href="ThirdPage.html">Third Page</a> 
-                <a href="FourthPage.html">Fourth Page</a> 
-                <a class="active" href="FifthPage.html">
+                <a href="Webpage1.php">Home page</a>
+                <a href="FirstPage.php">First Page</a> 
+                <a href="SecondPage.php">Second Page</a> 
+                <a href="ThirdPage.php">Third Page</a> 
+                <a href="FourthPage.php">Fourth Page</a> 
+                <a class="active" href="FifthPage.php">
                   Fifth Page
                 </a> 
             </nav>
@@ -103,56 +115,101 @@
             </aside>
           </div>
         <!-- paragraph the informs about the site's purpose -->
-        <p>This is the page where I made a calendar.</p>
+        <p>This is the page for the calendar.</p>
         <div class='container'>
           <div class='calendar'>
-              <div class='date-header'>
-                  <span id='prev' class='time-skip'>prev</span>
-                  <div class='year-month'>
-                      <span class='month'></span>
-                      <span class='year'></span>
-                  </div>
-                  <span id='next' class='time-skip'>next</span>
-              </div>
-              <table class='days-body'>
-                  <thead>
-                      <tr class='days-row'>
-                          <th>Sun</th>
-                          <th>Mon</th>
-                          <th>Tue</th>
-                          <th>Wed</th>
-                          <th>Thu</th>
-                          <th>Fri</th>
-                          <th>Sat</th>
-                      </tr>
-                  </thead>
-                  <tbody class='calendar-days'></tbody>
-              </table>
-              <form class='select-date'>
-                  <label for='month'>Go To: </label>
-                  <select name='month' id='month'>
-                      <option value=0>Jan</option>
-                      <option value=1>Feb</option>
-                      <option value=2>Mar</option>
-                      <option value=3>Apr</option>
-                      <option value=4>May</option>
-                      <option value=5>Jun</option>
-                      <option value=6>Jul</option>
-                      <option value=7>Aug</option>
-                      <option value=8>Sep</option>
-                      <option value=9>Oct</option>
-                      <option value=10>Nov</option>
-                      <option value=11>Dec</option>
-                  </select>
-                  <input type='number' min='1' id='year'>
-              </form>
+          <?php
+          function buildHTMLTable($left, $center, $right, $arrange) {
+    $width = 400;
+    $tabs = "<table border=1 width=".$width.">";
+    $heading ="<table border=0 width=".$width.">".
+            "<tr>".
+            "<td>".
+            $left.
+            "</td><td>".
+            $center.
+            "</td><td>".
+            $right.
+            "</td>".
+            "</tr>".
+            "</table>";
+    $tabs = $tabs."<caption>".$heading."</caption>";
+    foreach($arrange as $i) {
+        $tabs = $tabs."<tr>";
+        foreach($i as $j) {
+            $tabs = $tabs."<td>".$j."</td>";
+        }
+        $tabs = $tabs."</tr>";
+    }
+    $tabs = $tabs."</table>";
+    
+    return $tabs;
+}
+$current_time = time();
+$year = $_GET['y'];
+$current_year = date('Y', $current_time);
+if(empty($year)) {
+    $year = $current_year;
+}
+$month = $_GET['m'];
+$current_month = date('n');
+if(empty($month)) {
+    $month = $current_month;
+}
+$month_name = date('F', mktime(0, 0, 0, $month, 10));
+$show_current_day = ($month == $current_month) && ($year == $current_year);
+$current_day = date('d', $current_time);
+$dates = array(array("S","M","T","W","T","F","S"));
+$columns = 5;
+$rows = 7;
+$days_in_month = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+$day_change = date('w', strtotime("$year-$month-01"));
+$n = 1;
+for($i=1; $i<=$columns; $i++) {
+    $row = array();
+    for($j=1; $j<=$rows; $j++) {
+        $insert = $n - $day_change;
+        if($insert > 0 && $insert <= $days_in_month) {
+            if($show_current_day &&
+               $insert == $current_day) {
+                array_push($row, "<b>".$insert."</b>");
+            } else {
+                array_push($row, strval($insert));
+            }
+        } else {
+            array_push($row, "");
+        }
+        
+        $n++;
+    }
+    array_push($dates, $row);
+}
+$url = $_SERVER['REQUEST_URI'];
+$url = strtok($url, '?');
+$choose_last_year = $year;
+$choose_last_month = $month-1;
+$choose_next_year = $year;
+$choose_next_month = $month+1;
+if($month == 1) {
+    $choose_last_month = 12;
+    $choose_last_year--;
+}
+if($month == 12) {
+    $choose_next_month = 1;
+    $choose_next_year++;
+}
+$choose_last = $url."?y=".$choose_last_year."&m=".$choose_last_month;
+$choose_next = $url."?y=".$choose_next_year."&m=".$choose_next_month;
+$pick_last = "<a href=".$choose_last.">&lt;Last&lt;</a>";
+$pick_next = "<a href=".$choose_next.">&gt;Next&gt;</a>";
+$heading = "<b>".$month_name." ".$year."</b>";
+echo buildHTMLTable($pick_last, $heading, $pick_next, $dates);
+?>
           </div>
       </div>
         <script src="CalendarFunction.js"></script>
-        
-
-        
         </div>
+        
         <!--footer-->
         <div class="footer">
           <footer>
@@ -162,7 +219,6 @@
               </p>
           </footer>
         </div>
-
     </body>
    
     </html>
